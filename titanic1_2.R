85947c549f89075f102d6cc6b667a6209c507fec
Samantha Lui <sluimathematics@gmail.com>
2014-10-08 00:35:49 -0700

    titanic1_2.R: ClassPort_table

--- titanic1_2.R
+++ titanic1_2.R
@@ -23,23 +23,53 @@ inTrain <- createDataPartition(y=titanic$Survived, p=0.8, list=FALSE)
 training <- titanic[inTrain,]
 testing <- titanic[-inTrain,]
 
+plot(Pclass~Embarked, data=training)
 
-xyplot(Survived~Age|Pclass, training)
-xyplot(Survived~Age|Embarked, training)
+male <- which(training$Sex=="male")
+female <- which(training$Sex=="female")
+first <- which(training$Pclass==1)
+second <- which(training$Pclass==2)
+third <- which(training$Pclass==3)
+cherbourg <- which(training$Embarked=="C")
+queenstown <- which(training$Embarked=="Q")
+southampton <- which(training$Embarked=="S")
 
-firstM <- training[training$Pclass==1 & training$Sex=="male", ]
-secondM <- training[training$Pclass==2 & training$Sex=="male", ]
-thirdM <- training[training$Pclass==3 & training$Sex=="male", ]
-firstF <- training[training$Pclass==1 & training$Sex=="female", ]
-secondF <- training[training$Pclass==2 & training$Sex=="female", ]
-thirdF <- training[training$Pclass==3 & training$Sex=="female", ]
 
-cherbourgM <- training[training$Embarked=="C" & training$Sex=="male", ]
-queenstownM <- training[training$Embarked=="Q" & training$Sex=="male", ]
-southamptonM <- training[training$Embarked=="S" & training$Sex=="male", ]
-cherbourgF <- training[training$Embarked=="C" & training$Sex=="female", ]
-queenstownF <- training[training$Embarked=="Q" & training$Sex=="female", ]
-southamptonF <- training[training$Embarked=="S" & training$Sex=="female", ]
+firstM <- training[intersect(male,first), ]
+secondM <- training[intersect(male,second), ]
+thirdM <- training[intersect(male,third), ]
+firstF <- training[intersect(female,first), ]
+secondF <- training[intersect(female,second), ]
+thirdF <- training[intersect(female,third), ]
+
+cherbourgM <- training[intersect(male,cherbourg), ]
+queenstownM <- training[intersect(male,queenstown), ]
+southamptonM <- training[intersect(male,southampton), ]
+cherbourgF <- training[intersect(female,cherbourg), ]
+queenstownF <- training[intersect(female,queenstown), ]
+southamptonF <- training[intersect(female,southampton), ]
+
+m_1_ch <- training[intersect(intersect(male,first),cherbourg), ]
+m_1_qu <- training[intersect(intersect(male,first),queenstown), ]
+m_1_so <- training[intersect(intersect(male,first),southampton), ]
+m_2_ch <- training[intersect(intersect(male,second),cherbourg), ]
+m_2_qu <- training[intersect(intersect(male,second),queenstown), ]
+m_2_so <- training[intersect(intersect(male,second),southampton), ]
+m_3_ch <- training[intersect(intersect(male,third),cherbourg), ]
+m_3_qu <- training[intersect(intersect(male,third),queenstown), ]
+m_3_so <- training[intersect(intersect(male,third),southampton), ]
+
+f_1_ch <- training[intersect(intersect(female,first),cherbourg), ]
+f_1_qu <- training[intersect(intersect(female,first),queenstown), ]
+f_1_so <- training[intersect(intersect(female,first),southampton), ]
+f_2_ch <- training[intersect(intersect(female,second),cherbourg), ]
+f_2_qu <- training[intersect(intersect(female,second),queenstown), ]
+f_2_so <- training[intersect(intersect(female,second),southampton), ]
+f_3_ch <- training[intersect(intersect(female,third),cherbourg), ]
+f_3_qu <- training[intersect(intersect(female,third),queenstown), ]
+f_3_so <- training[intersect(intersect(female,third),southampton), ]
+
+
 
 fm <- sum(firstM$Survived==1)/nrow(firstM)
 sm <- sum(secondM$Survived==1)/nrow(secondM)
@@ -55,11 +85,37 @@ cf <- sum(cherbourgF$Survived==1)/nrow(cherbourgF)
 qf <- sum(queenstownF$Survived==1)/nrow(queenstownF)
 sof <- sum(southamptonF$Survived==1)/nrow(southamptonF)
 
+m_1_chR <- sum(m_1_ch$Survived==1)/nrow(m_1_ch)
+m_1_quR <- sum(m_1_qu$Survived==1)/nrow(m_1_qu)
+m_1_soR <- sum(m_1_so$Survived==1)/nrow(m_1_so)
+m_2_chR <- sum(m_2_ch$Survived==1)/nrow(m_2_ch)
+m_2_quR <- sum(m_2_qu$Survived==1)/nrow(m_2_qu)
+m_2_soR <- sum(m_2_so$Survived==1)/nrow(m_2_so)
+m_3_chR <- sum(m_3_ch$Survived==1)/nrow(m_3_ch)
+m_3_quR <- sum(m_3_qu$Survived==1)/nrow(m_3_qu)
+m_3_soR <- sum(m_3_so$Survived==1)/nrow(m_3_so)
+
+f_1_chR <- sum(f_1_ch$Survived==1)/nrow(f_1_ch)
+f_1_quR <- sum(f_1_qu$Survived==1)/nrow(f_1_qu)
+f_1_soR <- sum(f_1_so$Survived==1)/nrow(f_1_so)
+f_2_chR <- sum(f_2_ch$Survived==1)/nrow(f_2_ch)
+f_2_quR <- sum(f_2_qu$Survived==1)/nrow(f_2_qu)
+f_2_soR <- sum(f_2_so$Survived==1)/nrow(f_2_so)
+f_3_chR <- sum(f_3_ch$Survived==1)/nrow(f_3_ch)
+f_3_quR <- sum(f_3_qu$Survived==1)/nrow(f_3_qu)
+f_3_soR <- sum(f_3_so$Survived==1)/nrow(f_3_so)
+
+
+
+
 classPort <- data.frame(
-                male=c(fm,sm,tm,cm,qm,som),
-                female=c(ff,sf,tf,cf,qf,sof)
+                male=c(fm,sm,tm,cm,qm,som,m_1_chR,m_1_quR,m_1_soR,m_2_chR,m_2_quR,
+                       m_2_soR,m_3_chR,m_3_quR,m_3_soR),
+                female=c(ff,sf,tf,cf,qf,sof,f_1_chR,f_1_quR,f_1_soR,f_2_chR,f_2_quR,
+                         f_2_soR,f_3_chR,f_3_quR,f_3_soR)
         )
-classPort <- cbind(CP=c('C1','C2','C3','PChe','PQuw','PSou'),classPort)
+classPort <- cbind(CP=c('1','2','3','Ch','Qu','So','1_ch','1_qu','1_so',
+                        '2_ch','2_qu','2_so','3_ch','3_qu','3_so'),classPort)
 
 
 
